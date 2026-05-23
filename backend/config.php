242fb4ea2a2db5bffa32ca540e65fbaf0c27768c
iballacarlo <carlo.iballa@cvsu.edu.ph>
2026-05-24 00:08:02 +0800

    Fix database host socket error by changing localhost to 127.0.0.1

--- backend/config.php
+++ backend/config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-  'db_host' => getenv('MYSQLHOST') ?: 'localhost',     
+  'db_host' => getenv('MYSQLHOST') ?: '127.0.0.1',     
   'db_name' => getenv('MYSQLDATABASE') ?: 'barangay_db',   
   'db_user' => getenv('MYSQLUSER') ?: 'root',          
   'db_pass' => getenv('MYSQLPASSWORD') ?: '',              
